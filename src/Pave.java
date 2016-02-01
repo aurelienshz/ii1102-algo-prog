6b382b5fdb88cce81140536bae1fd44d0281b7c7
aurelienshz <aurelien.schiltz@free.fr>
2016-02-01 11:29:54 +0100

    Initial commit

--- src/Pave.java
+++ src/Pave.java
@@ -1,11 +1,39 @@
 import java.util.Scanner;
 
-
 public class Pave {
-    public static void main(String ... args) {
+    public static void main(String [] args) {
 
-	//1. Demander chaque longueur grâce au Scanner
-	Scanner scan = new Scanner(System.in);
-	
+        Scanner scan = new Scanner(System.in);
+
+        double l = -1;
+        double w = -1;
+        double h = -1;
+
+        System.out.print("Hauteur du pavé : ");
+        h = scan.nextDouble();
+
+        while(h<=0) {
+            System.out.print("Entrez un nombre valide : ");
+            h = scan.nextDouble();
+        }
+
+        System.out.print("Hauteur du pavé : ");
+        h = scan.nextDouble();
+
+        while(h<=0) {
+            System.out.print("Entrez un nombre valide : ");
+            h = scan.nextDouble();
+        }
+
+        System.out.print("Hauteur du pavé : ");
+        h = scan.nextDouble();
+
+        while(h<=0) {
+            System.out.print("Entrez un nombre valide : ");
+            h = scan.nextDouble();
+        }
+
+        
+        System.out.println("Volume du pavé : " + w*h*l);
     }
 }
